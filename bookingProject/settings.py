1e646db017ac36690f5d9e1b8ea2f2724e50c55c
gerald <darkobekoegerald2@gmail.com>
2024-04-14 14:54:50 +0000

    some comment made

--- bookingProject/settings.py
+++ bookingProject/settings.py
@@ -127,4 +127,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+
+#This last one might have impact on database schedule manually
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
